0cfa383c1d57bba0845172caaa65fd3a0319260a
App Generator <51070104+app-generator@users.noreply.github.com>
2025-07-26 01:09:26 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Test(models.Model):
+
+    #__Test_FIELDS__
+    фио = models.CharField(max_length=255, null=True, blank=True)
+    номер заказчика = models.IntegerField(null=True, blank=True)
+    облс = models.CharField(max_length=255, null=True, blank=True)
+    город = models.CharField(max_length=255, null=True, blank=True)
+    село или район = models.CharField(max_length=255, null=True, blank=True)
+    индекс = models.IntegerField(null=True, blank=True)
+    фио мамы или уч = models.CharField(max_length=255, null=True, blank=True)
+    номер мамы или уч = models.IntegerField(null=True, blank=True)
+    фио получателья = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Test_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Test")
+        verbose_name_plural = _("Test")
+
+
+
+#__MODELS__END
